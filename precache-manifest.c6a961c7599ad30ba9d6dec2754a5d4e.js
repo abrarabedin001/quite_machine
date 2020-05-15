a465201eb71bbf2d924dfa1b0dfcde4dd8469037
abrarabedin001 <abrarabedin001@gmail.com>
2020-05-15 10:05:06 +0600

    Updates

---
+++ precache-manifest.c6a961c7599ad30ba9d6dec2754a5d4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c66b2944110044ebd03a36bc3c8a9534",
+    "url": "/quite_machine/index.html"
+  },
+  {
+    "revision": "7344b0c5f39c1c5cd313",
+    "url": "/quite_machine/static/css/main.29fc8c0b.chunk.css"
+  },
+  {
+    "revision": "a440bdf3f795dbd9dfc8",
+    "url": "/quite_machine/static/js/2.fb7f579a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/quite_machine/static/js/2.fb7f579a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7344b0c5f39c1c5cd313",
+    "url": "/quite_machine/static/js/main.78a4a445.chunk.js"
+  },
+  {
+    "revision": "ccc5a978f9ba277f969e",
+    "url": "/quite_machine/static/js/runtime-main.91c8b9d9.js"
+  }
+]);
